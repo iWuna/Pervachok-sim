95564dbc7a309069e6bb83cd29ee2ae419265d08
NotAMerens <v01d.owl.merens@gmail.com>
2022-09-19 22:44:47 +0600

    THE START

---
+++ sound_module.js
@@ -0,0 +1,9 @@
+function sound(){
+     var snd = new Audio('audio/student1.mp3')//wav is also supported
+     snd.play()//plays the sound
+}
+
+function whip(){
+     var snd = new Audio('audio/whip.mp3')//wav is also supported
+     snd.play()//plays the sound
+}
